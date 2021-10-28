756df05044a912ac45b50c4abbd3150812295b02
skinksteek <linusigelstrom@gmail.com>
2021-10-28 15:51:48 +0200

    added express static

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,6 +14,7 @@ const PORT = process.env.PORT || 8000
 
 
 // middleware
+app.use( express.static(__dirname + '/../build') )
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(cors())
