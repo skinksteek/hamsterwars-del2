bd1458b2ed80b237368bc06e5a1b0f54d392ad60
skinksteek <linusigelstrom@gmail.com>
2021-11-03 19:49:57 +0100

    asd

--- backend/src/server.js
+++ backend/src/server.js
@@ -24,8 +24,9 @@ app.use((req, res, next) => {
 })
 
 // routes / endpoints
-app.get('*', (req, res) => {
-	res.sendFile(__dirname + '../../build/index.html')
+app.get('/', (req, res) => {
+	// res.sendFile(__dirname + '../../build/index.html')
+  // console.log(__dirname)
 })
 app.get('/', (req, res) => {
   console.log('Web root')
