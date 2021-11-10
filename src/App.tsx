6e44fe6218903984459fbc89d562327b895f86f2
skinksteek <linusigelstrom@gmail.com>
2021-11-10 17:58:44 +0100

    Fix indentation

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,28 @@ import { Link, Switch, Route, BrowserRouter as Router } from 'react-router-dom'
 import Start from './components/Start';
 import './App.css';
 import Gallery from './components/Gallery';
+import Compete from './components/Compete';
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <header>
-          <nav>
-            <Link to="/">Start</Link>
-            <Link to="/compete">Tävla</Link>
-            <Link to="/gallery">Galleri</Link>
-          </nav>
-        </header>
-        <main>
-          <Switch>
-            <Route path="/" exact><Start /></Route>
-            <Route path="/gallery"> <Gallery /></Route>
-          </Switch>
-        </main>
-      </Router>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Router>
+      <header>
+        <nav>
+          <Link to="/">Start</Link>
+          <Link to="/compete">Tävla</Link>
+          <Link to="/gallery">Galleri</Link>
+        </nav>
+
+      </header>
+      <main>
+        <Switch>
+          <Route path="/" exact><Start /></Route>
+          <Route path="/gallery"><Gallery /></Route>
+          <Route path="/compete"><Compete /></Route>
+        </Switch>
+      </main>
+    </Router>
+  </div>
+);
 
 export default App;
